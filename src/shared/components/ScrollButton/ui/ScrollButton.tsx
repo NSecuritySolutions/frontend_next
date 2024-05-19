b6f1fb2eb8d4c9d1375c1a08adea4d0ed4f5b6f1
Aleksey <129634706+aleksey2299-1@users.noreply.github.com>
2024-05-19 04:52:28 +0300

    Added scroll button

---
+++ src/shared/components/ScrollButton/ui/ScrollButton.tsx
@@ -0,0 +1,88 @@
+import Image from 'next/image'
+import { Typography } from '../../CalculatorCard/ui/styled'
+import colors from '@/shared/constants/colors'
+import { useEffect, useRef, useState } from 'react'
+import { FloatDiv } from './styled'
+import { AnimatePresence } from 'framer-motion'
+
+const ScrollButton = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [hasScrolledTwoScreens, setHasScrolledTwoScreens] = useState(false)
+  const [isFixed, setIsFixed] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      const screenHeight = window.innerHeight
+
+      // Проверяем, проскроллил ли пользователь два экрана
+      if (scrollPosition > 2 * screenHeight) {
+        setHasScrolledTwoScreens(true)
+      } else {
+        setHasScrolledTwoScreens(false)
+      }
+
+      const mainElement = document.querySelector('main')
+
+      if (mainElement && ref.current) {
+        const mainRect = mainElement.getBoundingClientRect()
+
+        console.log(mainElement.offsetTop + mainElement.offsetHeight, mainRect.bottom)
+
+        if (mainRect.bottom <= window.innerHeight) {
+          setIsFixed(false)
+        } else {
+          setIsFixed(true)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const handleOnClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  return (
+    <AnimatePresence>
+      {hasScrolledTwoScreens && (
+        <FloatDiv
+          ref={ref}
+          onClick={() => handleOnClick()}
+          position={isFixed ? 'fixed' : 'absolute'}
+          bottom={30}
+          right={40}
+          initial={{ bottom: '-80px' }}
+          animate={{ bottom: 30 }}
+          exit={{ bottom: '-80px' }}
+        >
+          <Image
+            src="/icons/arrow-up.svg"
+            alt="Вернуться в начало"
+            width={40}
+            height={40}
+            style={{
+              borderRadius: '50%',
+              backgroundColor: colors.backgroundPrimary,
+              padding: 7,
+            }}
+          />
+          {/* @TODO Не забыть убрать width, после мержа */}
+          <Typography size={13} width="fit-content">
+            наверх
+          </Typography>
+        </FloatDiv>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default ScrollButton
