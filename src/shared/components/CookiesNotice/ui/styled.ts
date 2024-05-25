a62f5a87a35a32ef85320a6acfb3f61c7776d6aa
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-05-26 01:35:58 +0300

    Feat: adaptive for cookie notice

--- src/shared/components/CookiesNotice/ui/styled.ts
+++ src/shared/components/CookiesNotice/ui/styled.ts
@@ -17,9 +17,37 @@ const CookiesContainer = styled.div`
   background-color: ${colors.backgroundPrimary};
   box-shadow: 2px 2px 25px 0 rgba(16, 16, 16, 0.05);
   z-index: 10;
+
   a:first-of-type {
     font-size: 13px;
     color: ${colors.titleBlueColor};
+  }
+
+  @media (max-width: 620px) {
+    padding: 30px;
+    max-width: 380px;
+    min-height: 190px;
+    gap: 12px;
+    left: 30px;
+
+    a:first-of-type {
+      font-size: 11px;
+    }
+
+    @media (max-width: 440px) {
+      padding: 16px;
+      max-width: 280px;
+      min-height: 150px;
+      gap: 10px;
+      left: 16px;
+      bottom: 16px;
+
+      a {
+      &:nth-of-type(2n){
+        width: 136px;
+        font-size: 13px;
+      }
+    }
   }
 `
 const CookiesTitle = styled.h3`
@@ -29,6 +57,12 @@ const CookiesTitle = styled.h3`
     sans-serif;
   color: black;
   color: ${colors.darkPrimary};
+
+  @media (max-width: 620px) {
+    font:
+      700 18px Manrope,
+      sans-serif;
+  }
 `
 
 const CookiesPragraph = styled.p`
@@ -36,6 +70,12 @@ const CookiesPragraph = styled.p`
     400 16px Manrope,
     sans-serif;
   color: ${colors.darkPrimary};
+
+  @media (max-width: 620px) {
+    font:
+      700 12px Manrope,
+      sans-serif;
+  }
 `
 
 const CloseButton = styled.button`
@@ -53,6 +93,18 @@ const CloseButton = styled.button`
   &:hover {
     opacity: 0.6;
     transition: opacity 0.1s ease-in;
+  }
+
+  @media (max-width: 620px) {
+    left: 345px;
+    img {
+      width: 15px;
+      height: 15px;
+    }
+  }
+
+  @media (max-width: 440px) {
+    left: 245px;
   }
 `
 
