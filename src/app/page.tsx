cdee355a18143d4dc68ea1152c7032bee0f08baa
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-05-27 15:10:41 +0300

    Feat: img style fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,20 @@ import { ExamplesSlider } from '@/widgets/ExamplesSlider'
 import { OurClients } from '@/widgets/OurClients'
 import { Calculator } from '@/widgets/Calculator'
 import { ContactForm } from '../widgets/ContactForm'
-
+import Cookies from 'js-cookie'
 import { ScrollButton } from '@/shared/components/ScrollButton'
 import { CookiesNotice } from '@/shared/components/CookiesNotice'
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
-
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false)
     }, 0)
   }, [])
+
+  // Cookies.set('guest', 'first-visit', { expires: 7 })
+  // Cookies.remove('')
 
   return (
     <>
