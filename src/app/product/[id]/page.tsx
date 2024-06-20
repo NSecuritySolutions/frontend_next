2a2781c2cbf1684031f4181e5ba4811d96ea547c
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-06-20 18:41:08 +0300

    Feat: product page dir

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { Product } from '@/widgets/Product'
+import { cardSolutionData } from '@/shared/constants/texts/cards-solution'
+
+import styles from './page.module.css'
+import { Breadcrumbs } from '@/shared/components/Breadcrumbs'
+
+export default function Page({ params }: { params: { id: string } }) {
+  const data = cardSolutionData.filter((solution) => solution.id?.toString() === params.id)[0]
+
+  return (
+    <main className={styles.main} id="product-item">
+      <Breadcrumbs title={data.title} />
+      {/* <Product data={data} /> */}
+    </main>
+  )
+}
