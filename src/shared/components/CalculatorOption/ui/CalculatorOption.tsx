17196e79be72718f4d08cad1f7c06090c8cf2e65
Aleksey <129634706+aleksey2299-1@users.noreply.github.com>
2024-07-11 11:52:43 +0300

    Added observer for calc option

--- src/shared/components/CalculatorOption/ui/CalculatorOption.tsx
+++ src/shared/components/CalculatorOption/ui/CalculatorOption.tsx
@@ -7,6 +7,7 @@ import { Tooltip } from '@/shared/components/Tooltip'
 import { RadioGroup } from '@/shared/components/RadioGroup'
 import { AmountComponent } from '@/shared/components/AmountComponent'
 import CalculatorBlockStore from '@/shared/components/CalculatorCard/store'
+import { observer } from 'mobx-react-lite'
 
 interface CalculatorOptionProps {
   option: IOption
@@ -15,49 +16,51 @@ interface CalculatorOptionProps {
   amount: number
 }
 
-const CalculatorOption: FC<CalculatorOptionProps> = ({ option, store, onChange, amount }) => {
-  const firstArgFunc = (name: string) => (value: number) => {
-    store.setVariable(name, value.toString())
-  }
+const CalculatorOption: FC<CalculatorOptionProps> = observer(
+  ({ option, store, onChange, amount }) => {
+    const firstArgFunc = (name: string) => (value: number) => {
+      store.setVariable(name, value.toString())
+    }
 
-  return (
-    <Option>
-      <OptionHeader>
-        <Typography size={13} width="100%" color={colors.textSecondary}>
-          {option.title}
-        </Typography>
-        <Tooltip text={option.description} />
-      </OptionHeader>
-      {option.option_type === 'radio' && (
-        <RadioGroup option={option} onChange={onChange} store={store} />
-      )}
-      {option.option_type === 'checkbox' && (
-        <CheckBox
-          tabIndex={amount == 0 ? -1 : 0}
-          checked={store.getVariable(option.name) as boolean}
-          onChange={(e) => {
-            onChange(option, () => store.setVariable(option.name, e.target.checked))
-          }}
-        />
-      )}
-      {option.option_type === 'number' && (
-        <InputNumber
-          tabIndex={amount == 0 ? -1 : 0}
-          value={store.getVariable(option.name) as number}
-          // onChange={(e) => onChange(option, () => store.setVariable(option.name, e.target.value))} // плохая идея
-          onChange={(e) => store.setVariable(option.name, e.target.value)}
-        />
-      )}
-      {option.option_type === 'counter' && (
-        <AmountComponent
-          amount={parseInt(store.getVariable(option.name) as string)}
-          // onChange={(value: number) => onChange(option, () => firstArgFunc(option.name)(value))}
-          onChange={firstArgFunc(option.name)}
-          small
-        />
-      )}
-    </Option>
-  )
-}
+    return (
+      <Option>
+        <OptionHeader>
+          <Typography size={13} width="100%" color={colors.textSecondary}>
+            {option.title}
+          </Typography>
+          <Tooltip text={option.description} />
+        </OptionHeader>
+        {option.option_type === 'radio' && (
+          <RadioGroup option={option} onChange={onChange} store={store} />
+        )}
+        {option.option_type === 'checkbox' && (
+          <CheckBox
+            tabIndex={amount == 0 ? -1 : 0}
+            checked={store.getVariable(option.name) as boolean}
+            onChange={(e) => {
+              onChange(option, () => store.setVariable(option.name, e.target.checked))
+            }}
+          />
+        )}
+        {option.option_type === 'number' && (
+          <InputNumber
+            tabIndex={amount == 0 ? -1 : 0}
+            value={store.getVariable(option.name) as number}
+            // onChange={(e) => onChange(option, () => store.setVariable(option.name, e.target.value))} // плохая идея
+            onChange={(e) => store.setVariable(option.name, e.target.value)}
+          />
+        )}
+        {option.option_type === 'counter' && (
+          <AmountComponent
+            amount={parseInt(store.getVariable(option.name) as string)}
+            // onChange={(value: number) => onChange(option, () => firstArgFunc(option.name)(value))}
+            onChange={firstArgFunc(option.name)}
+            small
+          />
+        )}
+      </Option>
+    )
+  },
+)
 
 export default CalculatorOption
