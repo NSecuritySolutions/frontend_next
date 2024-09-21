8e90d1d34336b5f3693e4004a9bd9f517c113ce9
Aleksey <129634706+aleksey2299-1@users.noreply.github.com>
2024-09-21 15:18:03 +0300

    Base url as environmental variable

--- src/shared/constants/url/url.ts
+++ src/shared/constants/url/url.ts
@@ -1,2 +1,2 @@
 export const YANDEX_URL = 'https://market.yandex.ru/'
-export const BASE_URL = 'https://nssolution.sytes.net' // Нужно получить из переменной окружения
+export const BASE_URL = process.env.BASE_URL
