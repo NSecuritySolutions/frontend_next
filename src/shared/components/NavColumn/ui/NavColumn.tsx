1bf7ca413e409dc2e561851af84452db2200b589
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-10-23 14:55:39 +0300

    fix: footer routing error fixed

--- src/shared/components/NavColumn/ui/NavColumn.tsx
+++ src/shared/components/NavColumn/ui/NavColumn.tsx
@@ -1,36 +1,3 @@
-// import { FC } from 'react'
-// import { usePathname } from 'next/navigation'
-
-// import { TNavColumnProps } from '../types/types'
-// import { Container, List, ListItem, NavLinkStyled, Title } from './styled'
-
-// const NavColumn: FC<TNavColumnProps> = ({ lists }) => {
-//   const pathname = usePathname()
-//   return (
-//     <Container>
-//       {lists.map((list, index) => (
-//         <div key={index}>
-//           <Title>{list.title}</Title>
-//           <List>
-//             {list.items.map((item, idx) => (
-//               <ListItem key={idx}>
-//                 <NavLinkStyled
-//                   href={item.navLink}
-//                   className={item.navLink === pathname ? 'active' : undefined}
-//                 >
-//                   {item.text}
-//                 </NavLinkStyled>
-//               </ListItem>
-//             ))}
-//           </List>
-//         </div>
-//       ))}
-//     </Container>
-//   )
-// }
-
-// export default NavColumn
-
 import { FC } from 'react'
 import { usePathname } from 'next/navigation'
 
--- src/widgets/Footer/ui/temporaryConsts.ts
+++ src/widgets/Footer/ui/temporaryConsts.ts
@@ -2,10 +2,10 @@ export const navColumnLists = [
   {
     title: 'Возможности',
     items: [
-      { text: 'Оставить заявку', navLink: `/#contact-form` },
-      { text: 'Готовые решения', navLink: '/#solutions' },
-      { text: 'Калькулятор', navLink: '/#calculator' },
-      { text: 'Загрузить спецификацию', navLink: '/#contact-form' },
+      { text: 'Оставить заявку', navLink: `#contact-form` },
+      { text: 'Готовые решения', navLink: '#solutions' },
+      { text: 'Калькулятор', navLink: '#calculator' },
+      { text: 'Загрузить спецификацию', navLink: '#contact-form' },
     ],
   },
   {
@@ -22,12 +22,12 @@ export const navColumnLists = [
   {
     title: 'Информация',
     items: [
-      { text: 'Наши услуги', navLink: '/#our-services' },
-      { text: 'Преимущества', navLink: '/#advantages' },
-      { text: 'Этапы работы', navLink: '/#project-stages' },
-      { text: 'Наша команда', navLink: '/#our-team' },
-      { text: 'Часто задаваемые вопросы', navLink: '/#faq' },
-      { text: 'Отзывы', navLink: '/#reviews' },
+      { text: 'Наши услуги', navLink: '#our-services' },
+      { text: 'Преимущества', navLink: '#advantages' },
+      { text: 'Этапы работы', navLink: '#project-stages' },
+      { text: 'Наша команда', navLink: '#our-team' },
+      { text: 'Часто задаваемые вопросы', navLink: '#faq' },
+      { text: 'Отзывы', navLink: '#reviews' },
       { text: 'Примеры наших работ', navLink: '/ourworks' },
       { text: 'Политика конфиденциальности', navLink: '/policy' },
     ],
